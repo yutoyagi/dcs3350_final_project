69a824b7966931077c3fd7ebc4787535e2b10e20
Yuto Yagi <35690865+yutoyagi@users.noreply.github.com>
2021-04-17 18:19:18 -0400

    add SEIR

--- main.py
+++ main.py
@@ -28,6 +28,72 @@ def SI(G, beta):
         #color_draw(G)
     return t
 
+
+
+def SEIR(G, beta):
+    # randomly infect one node
+    rand_idx = random.randrange(0, G.number_of_nodes())
+    G.nodes[rand_idx]['Infected'] = True
+    G.nodes[rand_idx]['Infected_t'] = 0
+
+    # run simulation
+    t = 0
+    while count_infected(G) <= (G.number_of_nodes()*0.36):
+
+"""
+        # S- >E
+        infected_nodes = [i for i in G.nodes if G.nodes[i]['Infected'] ]
+        for i in infected_nodes:
+            # for each neigbor
+            for nbr in G.neighbors(i):
+                G.nodes[nbr]['Exposed'] = True
+                if random.random() <= beta:
+                    # this neighbor is infected
+                    G.nodes[nbr]['Turn_I_at_t=x'] = t + random.randint(1,10) 
+                else:
+                    G.nodes[nbr]['Turn_S_at_t=x'] = t + 10
+"""
+
+        # E- >I, E->S, I->R
+        for i in G.nodes:
+            if G.nodes[i]['Exposed']: 
+                if G.nodes[nbr]['Turn_I_at_t=x'] and G.nodes[nbr]['Turn_I_at_t=x'] <= t:
+                    G.nodes[nbr]['Exposed'] = False
+                    G.nodes[nbr]['Infected'] = True
+                    G.nodes[nbr]['Infected_t'] = t
+                    G.nodes[nbr]['Turn_I_at_t=x'] = None
+                if G.nodes[nbr]['Turn_S_at_t=x'] and G.nodes[nbr]['Turn_I_at_t=x'] <= t:
+                    G.nodes[nbr]['Exposed'] = False
+                    G.nodes[nbr]['Suseptible'] = True
+                    G.nodes[nbr]['Turn_S_at_t=x'] = None
+
+
+            else if G.nodes[i]['Infected']: 
+                # infect others
+                for nbr in G.neighbors(i):
+                    if G.nodes[nbr]['Suseptible']:
+                        G.nodes[nbr]['Suseptible'] = False
+                        G.nodes[nbr]['Exposed'] = True
+                        if random.random() <= beta:
+                            # this neighbor is infected
+                            G.nodes[nbr]['Turn_I_at_t=x'] = t + random.randint(1,10) 
+                        else:
+                            G.nodes[nbr]['Turn_S_at_t=x'] = t + 10
+                # check if they should recover by now
+                if G.nodes[i]['Infected_t'] + 14 <= t:
+                    G.nodes[i]['Infected'] = False
+                    G.nodes[i]['Infected_t'] = None
+                    G.nodes[i]['Recovered'] = True
+                
+        t+=1
+        print(t)
+        if t >= 100000:
+            break
+        #color_draw(G)
+    return t
+
+
+
 def count_infected(G):
     cnt = 0
     for i in G.nodes:
@@ -51,10 +117,45 @@ def color_draw(G):
     nx.draw(G, node_color=color_map, with_labels=True)
     return None 
 
+# this function takes Graph G and beta value beta, and returns modified G and beta
+def hard_lockdown(G, beta):
+    # delete edges 
+    # reduce beta because of mask 
+    return (new_G, new_beta)
+
+# this function takes Graph G and beta value beta, and returns modified G and beta
+def soft_lockdown(G, beta):
+    # delete edges 
+    # reduce beta because of mask 
+    return (new_G, new_beta)
 
 # Wattz Strogartz network 
 
+output = [] # list of list [network_size, beta, avg_degree, time]
+n = 100     
+k = 5       # household's size
+p = 0.3     
 
+# lower beta with stronger lockdown
+
+G = nx.watts_strogatz_graph(n, k, p, seed=None)
+# TODO: add dissident node
+for i in G.nodes: 
+    G.nodes[i]['Suceptible'] = True
+    G.nodes[i]['Exposed'] = False
+    G.nodes[nbr]['Turn_I_at_t=x'] = None
+    G.nodes[nbr]['Turn_S_at_t=x'] = None
+    G.nodes[i]['Infected'] = False
+    G.nodes[i]['Infected_t'] = None
+    G.nodes[i]['Recovered'] = False
+
+avg_degree = get_avg_degree(G)
+time = SI(G, beta)
+output.append([network_size, beta, avg_degree, time])
+        
+with open("output.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(output)
 
 '''
 #Barabasi-Albert
